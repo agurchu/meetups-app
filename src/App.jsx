154d40b7041b494cf940d7314433afecd234f65a
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-04-01 15:46:01 +0200

    putting routes route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
+import AllMeetupsPage from "./assets/pages/AllMeetups";
+import NewMeetupPage from "./assets/pages/NewMeetup";
+import FavoritesPage from "./assets/pages/Favorites";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<AllMeetupsPage />} />
+      <Route path="/new-meetup" element={<NewMeetupPage />} />
+      <Route path="/favorites" element={<FavoritesPage />} />
+    </Routes>
+  );
 }
 
 export default App;
