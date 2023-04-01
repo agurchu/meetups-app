5f3d04917d7f6ae0417c490b15847a600b4fecd9
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-04-01 19:48:11 +0200

    MeetupItem

--- src/assets/pages/AllMeetups.jsx
+++ src/assets/pages/AllMeetups.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import MeetupItem from "../../components/meetups/MeetupItem";
+
+const Dummy_Data = [
+  {
+    id: "m1",
+    title: "This is a first meetup",
+    image:
+      "https://techboomers.com/wp-content/uploads/2017/11/sites-like-meetup-h.jpg",
+    address: "Mashifane park, 241 E-block, Burgersfort",
+    description:
+      "this is a first, amazing meetup ehich you definetely should not miss",
+  },
+  {
+    id: "m2",
+    title: "This is a second meetup",
+    image:
+      "https://techboomers.com/wp-content/uploads/2017/11/sites-like-meetup-h.jpg",
+    address: "Mashifane park, 241 E-block, Burgersfort",
+    description:
+      "this is a first, amazing meetup ehich you definetely should not miss",
+  },
+];
 
 const AllMeetupsPage = () => {
-  return <div>AllMeetupsPage</div>;
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <ul>
+        {Dummy_Data.map((meetup) => (
+          <MeetupItem
+            address={meetup.address}
+            key={meetup.id}
+            description={meetup.description}
+            title={meetup.title}
+            image={meetup.image}
+          />
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default AllMeetupsPage;
--- src/components/layout/MainNavigation.jsx
+++ src/components/layout/MainNavigation.jsx
@@ -1,26 +1,73 @@
 import React from "react";
-
+import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 const MainNavigation = () => {
   return (
-    <header>
-      <div>KJ Meetups</div>
+    <Header>
+      <Logo>
+        <img src="kj-dev-icon.png" alt="" />
+        <span>KJ Meetups</span>
+      </Logo>
       <nav>
         <ul>
           <li>
             <Link to="/">All Meetups</Link>
           </li>
           <li>
-            <Link to="/new-meetup">New Meetups</Link>
+            <Link to="/new-meetup">Add New Meetup</Link>
           </li>
           <li>
-            <Link to="/favorites">Fovorites</Link>
+            <Link to="/favorites">My Fovorites</Link>
           </li>
         </ul>
       </nav>
-    </header>
+    </Header>
   );
 };
 
 export default MainNavigation;
+
+const Header = styled.header`
+  background: var(--color-accent);
+  display: flex;
+  justify-content: space-between;
+  padding: 1.5rem 10rem;
+  align-items: center;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.3);
+
+  ul {
+    display: flex;
+
+    li {
+      margin-left: 3rem;
+    }
+
+    a {
+      color: var(--color-primary);
+      font-size: var(--fontSize-nm);
+      font-weight: 500;
+      letter-spacing: 1px;
+    }
+  }
+  height: 60px;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  height: 100%;
+  color: var(--color-text-03);
+  align-items: center;
+
+  span {
+    white-space: nowrap;
+    font-size: var(--fontSize-mid);
+    font-weight: 600;
+    margin-left: 0.5rem;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+`;
