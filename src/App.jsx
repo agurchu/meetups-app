e29f618c80e8b477942d5486ed2df56028f52ff1
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-04-02 08:52:22 +0200

    layout jsx and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import Layout from "./components/layout/Layout";
 import "./App.css";
 import AllMeetupsPage from "./assets/pages/AllMeetups";
 import NewMeetupPage from "./assets/pages/NewMeetup";
 import FavoritesPage from "./assets/pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <Router>
-      <MainNavigation />
-      <Routes>
-        <Route path="/" element={<AllMeetupsPage />} />
-        <Route path="/new-meetup" element={<NewMeetupPage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<AllMeetupsPage />} />
+          <Route path="/new-meetup" element={<NewMeetupPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
