e009154bd40f61006967bad82b136bb407f69cbc
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-04-02 09:25:11 +0200

    NewMeetupForm  jsx markup

--- src/assets/pages/NewMeetup.jsx
+++ src/assets/pages/NewMeetup.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
-  return <div>NewMeetupPage</div>;
+  return (
+    <div>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm />
+    </div>
+  );
 };
 
 export default NewMeetupPage;
