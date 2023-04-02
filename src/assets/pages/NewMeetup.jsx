2a688c3168406ecc73b1a428d9f071626aca5427
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-04-02 10:47:15 +0200

    data storage from newmeetupform and firebase POST

--- src/assets/pages/NewMeetup.jsx
+++ src/assets/pages/NewMeetup.jsx
@@ -2,10 +2,18 @@ import React from "react";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
+  function handleAddMeetup(meetupData) {
+    fetch("https://react-first-data-default-rtdb.firebaseio.com/meetups.json", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
   return (
     <div>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={handleAddMeetup} />
     </div>
   );
 };
--- src/components/meetups/NewMeetupForm.jsx
+++ src/components/meetups/NewMeetupForm.jsx
@@ -1,26 +1,61 @@
 import React from "react";
 import Card from "../ui/Card";
 import styles from "./newMeetupForm.module.css";
+import { useRef } from "react";
 
-const NewMeetupForm = () => {
+const NewMeetupForm = (props) => {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+
+    props.onAddMeetup(meetupData);
+  };
+
   return (
     <Card>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.control}>
           <label htmlFor="title">Meetup Title</label>
-          <input type="text" required id="title" />
+          <input type="text" required id="title" ref={titleInputRef} />
         </div>
         <div className={styles.control}>
           <label htmlFor="image">Meetup Image</label>
-          <input type="url" placeholder="https://..." required id="image" />
+          <input
+            type="url"
+            placeholder="https://..."
+            required
+            id="image"
+            ref={imageInputRef}
+          />
         </div>
         <div className={styles.control}>
           <label htmlFor="address">Addres</label>
-          <input type="text" required id="address" />
+          <input type="text" required id="address" ref={addressInputRef} />
         </div>
         <div className={styles.control}>
           <label htmlFor="description">Description</label>
-          <textarea rows="5" required id="description"></textarea>
+          <textarea
+            rows="5"
+            required
+            id="description"
+            ref={descriptionInputRef}
+          ></textarea>
         </div>
         <div className={styles.actions}>
           <button>Add Meetup</button>
